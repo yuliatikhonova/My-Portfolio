ecc677c59fd438b62d7db93b4a0d6a777531a3e0
yulia tikhonova <yuliatikhonova@outlook.com>
2020-08-18 21:48:55 -0700

    installed react and files. Created the file structure. Created the sidebar

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import './App.css';
+
+function App() {
+  return (
+    <Sidebar />
+    
+
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Image from "../../images/20200701_214128.jpg";
+import "./style.css";
+
+function Sidebar() {
+    return (
+        <div id="wrapper">
+
+            <div id="sidebar-wrapper">
+                <ul class="sidebar-nav">
+                <br></br>
+                    <li>
+                        <img src={Image} />
+                    </li>
+                    <br></br>
+                    <li>
+                        <h1>Portfolio</h1>
+                        <h5>by Yulia Tikhonova</h5>
+                    </li>
+                    <br></br>
+                    <hr />
+                    <br></br>
+                    <li>
+                        <a href="#">Contact Me</a>
+                    </li>
+                    <li>
+                        <a href="#">GitHub</a>
+                    </li>
+                    <li>
+                        <a href="#">LinkedIn</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+
+
+    );
+}
+
+export default Sidebar;
