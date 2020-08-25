44a966a67964061acf67230f23eb5984c3bd6537
yulia tikhonova <yuliatikhonova@outlook.com>
2020-08-24 23:40:02 -0700

    Updates

---
+++ precache-manifest.9972497c93cde8a8a05443e624e173c0.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10a0b0ff2ee298438f95c6eaaff0d312",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "c58b26b0cb85d3d89ae9",
+    "url": "/My-Portfolio/static/css/2.81b7155e.chunk.css"
+  },
+  {
+    "revision": "1bcd8525c1214228ccff",
+    "url": "/My-Portfolio/static/css/main.2e675511.chunk.css"
+  },
+  {
+    "revision": "c58b26b0cb85d3d89ae9",
+    "url": "/My-Portfolio/static/js/2.9de8add9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-Portfolio/static/js/2.9de8add9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bcd8525c1214228ccff",
+    "url": "/My-Portfolio/static/js/main.fd641577.chunk.js"
+  },
+  {
+    "revision": "bd26aa8a63ab866d9120",
+    "url": "/My-Portfolio/static/js/runtime-main.295a29ee.js"
+  },
+  {
+    "revision": "c4b38ea52db4d9ac7edc9392e7cfdf8a",
+    "url": "/My-Portfolio/static/media/20200701_214128.c4b38ea5.jpg"
+  },
+  {
+    "revision": "d5fcc8caaa83900a03394b323ee06251",
+    "url": "/My-Portfolio/static/media/Code Quiz.d5fcc8ca.png"
+  },
+  {
+    "revision": "20e486c822ae656661d91f209fffe1dc",
+    "url": "/My-Portfolio/static/media/Lyrics to Go.20e486c8.png"
+  },
+  {
+    "revision": "5fc0098f1e2bd5c14d952a4fc51b0a33",
+    "url": "/My-Portfolio/static/media/Screen Shot 2020-07-02 at 12.50.47 AM.5fc0098f.png"
+  },
+  {
+    "revision": "b21ff62048714d65cf00f5d6af256b28",
+    "url": "/My-Portfolio/static/media/Screen Shot 2020-07-25 at 8.07.05 PM.b21ff620.png"
+  },
+  {
+    "revision": "ddfa96b7ff81abffbfad3dccdfdaf1ca",
+    "url": "/My-Portfolio/static/media/TukTuk.ddfa96b7.png"
+  },
+  {
+    "revision": "e842cae2dd0cd4b63941cfc35e05081a",
+    "url": "/My-Portfolio/static/media/Work Day Scheduler.e842cae2.png"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/My-Portfolio/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/My-Portfolio/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/My-Portfolio/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/My-Portfolio/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/My-Portfolio/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/My-Portfolio/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/My-Portfolio/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/My-Portfolio/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/My-Portfolio/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/My-Portfolio/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/My-Portfolio/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/My-Portfolio/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/My-Portfolio/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/My-Portfolio/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/My-Portfolio/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
